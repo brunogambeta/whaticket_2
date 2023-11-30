6862b4ee94eee9c0a02f78f91b236d7061f39a70
andre890074 <sandreo_goncalves@outlook.com>
2023-11-29 23:25:53 -0300

    Mensagem do seu commit aqui

---
+++ frontend/build/precache-manifest.f3da0458ed782764ae3fec057d3eaf54.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588e3745a3dde22b9788bb8d492ef32d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f83102c44e87509bf1c",
+    "url": "/static/css/2.00597995.chunk.css"
+  },
+  {
+    "revision": "67e9ec9660e4de80c2ba",
+    "url": "/static/css/main.f18ab2b1.chunk.css"
+  },
+  {
+    "revision": "8f83102c44e87509bf1c",
+    "url": "/static/js/2.23e072f0.chunk.js"
+  },
+  {
+    "revision": "5c95ec5d90c25e5494c648c0798f4148",
+    "url": "/static/js/2.23e072f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531573a0c84950ccdc74",
+    "url": "/static/js/3.acff2fe7.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/3.acff2fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e9ec9660e4de80c2ba",
+    "url": "/static/js/main.3181ba72.chunk.js"
+  },
+  {
+    "revision": "af06cafa39d2b002602b",
+    "url": "/static/js/runtime-main.1542487b.js"
+  },
+  {
+    "revision": "2c07b0d6219e78fb6311323de22a59a9",
+    "url": "/static/media/bling.2c07b0d6.png"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "e6eaa98dd6ccecec3d3c990cb95ea57d",
+    "url": "/static/media/hinova.e6eaa98d.png"
+  },
+  {
+    "revision": "b801b20b421f5c72fc24dbb1e9db3ca6",
+    "url": "/static/media/logo.b801b20b.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "705244415b5c0ceca604e82d22b35f1d",
+    "url": "/static/media/siprov.70524441.png"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.8244f042.png"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/wa-background.adbccee0.png"
+  }
+]);
